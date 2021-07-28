87632be52395f4fde5d070115e3cde5f58f90848
Anton Kesy <anton@kesy.de>
2021-07-28 21:30:32 +0200

    set statusBarColor & comments

--- app/src/main/java/com/antonkesy/customercounter/MainActivity.kt
+++ app/src/main/java/com/antonkesy/customercounter/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.antonkesy.customercounter
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
@@ -81,38 +82,45 @@ class MainActivity : AppCompatActivity() {
         stopTV = findViewById(R.id.stopTV)
         limitTV = findViewById(R.id.limitReachedTV)
 
+
+
     }
 
 
     private fun changeCustomerAmount(value: Long) {
         val newAmount = amountOfCustomers + value
+        //check if new amount is legal
         if (newAmount <= (maxAmount) && newAmount >= 0) {
             amountOfCustomers = newAmount
         }
-
+        //get new color
+        val newColor = when {
+            amountOfCustomers >= maxAmount -> {
+                R.color.red
+            }
+            amountOfCustomers >= maxAmount * .9 -> {
+                R.color.yellow
+            }
+            else -> {
+                R.color.green
+            }
+        }
         //set text color
         amountTV.setTextColor(
             ContextCompat.getColor(
-                this, when {
-                    amountOfCustomers >= maxAmount -> {
-                        R.color.red
-                    }
-                    amountOfCustomers >= maxAmount * .9 -> {
-                        R.color.yellow
-                    }
-                    else -> {
-                        R.color.green
-                    }
-                }
+                this, newColor
             )
         )
-
+        //set statusBar color
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(this, newColor)
+        }
         //set ui warnings
         val isFull = amountOfCustomers >= maxAmount
         customerIcon.visibility = if (isFull) View.GONE else View.VISIBLE
         stopTV.visibility = if (isFull) View.VISIBLE else View.GONE
         limitTV.visibility = if (isFull) View.VISIBLE else View.GONE
-
+        //set value in textView
         amountTV.text = (amountOfCustomers.toString() + "\\" + maxAmount.toString())
     }
 }
