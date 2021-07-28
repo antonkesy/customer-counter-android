a6209c23c51940a50923633592f550618f97a2f7
Anton Kesy <anton@kesy.de>
2021-07-28 20:04:20 +0200

    switch custom settings for root_preferences

--- app/src/main/java/com/antonkesy/customercounter/MainActivity.kt
+++ app/src/main/java/com/antonkesy/customercounter/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.antonkesy.customercounter
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<ImageButton>(R.id.settingsBtn).setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
     }
 }
