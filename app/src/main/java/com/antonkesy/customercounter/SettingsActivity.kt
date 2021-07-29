010901aebabfdb1ffdc5864436aa0e46bec93481
Anton Kesy <anton@kesy.de>
2021-07-29 09:41:52 +0200

    change preferences input to number only

--- app/src/main/java/com/antonkesy/customercounter/SettingsActivity.kt
+++ app/src/main/java/com/antonkesy/customercounter/SettingsActivity.kt
@@ -1,8 +1,11 @@
 package com.antonkesy.customercounter
 
 import android.os.Bundle
+import android.text.InputType
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
+
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -21,6 +24,22 @@ class SettingsActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+            //set number edit pref to only number dial
+            setNumberInputPref(preferenceManager.findPreference("customerKey")!!)
+            setNumberInputPref(preferenceManager.findPreference("maxKey")!!)
+
+        }
+
+        /**
+         * changes EditTextPreference input to number only
+         */
+        private fun setNumberInputPref(editTextPreference: EditTextPreference) {
+            editTextPreference.setOnBindEditTextListener { editText ->
+                editText.inputType =
+                    InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_SIGNED
+            }
         }
     }
+
+
 }
